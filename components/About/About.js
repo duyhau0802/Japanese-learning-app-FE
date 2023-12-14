9679efba8f32b05873b6c62e71942c5d9e50e483
Daiki <hothanhhung131@gmail.com>
2023-12-14 23:09:37 +0700

    add payment func

--- components/About/About.js
+++ components/About/About.js
@@ -1,14 +1,18 @@
 // AboutScreen.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button, Image } from "react-native";
 import { useNavigation } from "@react-navigation/core";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import axios  from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useStripe } from '@stripe/stripe-react-native';
 
 const AboutScreen = ({ courseData }) => {
   if (!courseData) {
     return;
   }
   const [showFullDescription, setShowFullDescription] = useState(false);
+  const [isPayment,setIsPayment] = useState(false);
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
   const navigation = useNavigation();
 
   const toggleDescription = () => {
@@ -22,7 +26,7 @@ const AboutScreen = ({ courseData }) => {
 
     return (
       <View>
-        <Text style={{ marginTop: 10, fontSize: 20, color: "gray" }}>
+        <Text style={{ marginTop: 10 }}>
           {showFullDescription ? courseData.description : truncatedDescription}
           {!showFullDescription &&
             courseData.description &&
@@ -36,68 +40,78 @@ const AboutScreen = ({ courseData }) => {
     );
   };
 
-  const enrollNow = () => {
-    // Chuyển đến màn hình thanh toán và truyền tham số
-    navigation.navigate("Payment", {
-      price: courseData.price,
-      name: courseData.title,
-    });
+  const enrollNow = async () => {
+    console.log(courseData.price)
+      try {
+        const res = await axios.post(
+          'http://54.164.6.175:3000/api/payment/',
+          {
+            amount: courseData.price*100,
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${await AsyncStorage.getItem('token')}`
+            }
+          }
+        )
+        const initResponse = await initPaymentSheet({
+          merchantDisplayName: 'ThanhHung',
+          paymentIntentClientSecret: res.data.paymentIntent,
+        });
+        if (initResponse.error) {
+          console.log(initResponse.error);
+          alert('Something went wrong');
+          return;
+        }
+        const paymentResponse = await presentPaymentSheet();
+        if (paymentResponse.error) {
+          alert(
+            `Error code: ${paymentResponse.error.code}`,
+            paymentResponse.error.message
+          );
+          return;
+        }
+        setIsPayment(true)
+      } catch (error) {
+        console.log(error)
+      }
   };
+
+
 
   return (
     <View style={{ padding: 20 }}>
-      <Text style={{ fontSize: 35, fontWeight: "bold" }}>
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>
         {courseData.title}
       </Text>
 
       {renderDescription()}
 
-      <Text style={{ fontSize: 26, marginTop: 10, fontWeight: "bold" }}>
-        教師
-      </Text>
+      <Text style={{ marginTop: 10, fontWeight: "bold" }}>Teacher</Text>
 
       <View style={{ flexDirection: "row", marginTop: 10 }}>
         <View style={{ flex: 1 }}>
-          <Text style={{ fontSize: 16, margin: 5 }}>{`名前: ${
+          <Text>{`Name: ${
             courseData.Teacher && courseData.Teacher.User.nick_name
           }`}</Text>
-          <Text style={{ fontSize: 16, margin: 5 }}>{`日本語のレベル: ${
+          <Text>{`Japanese Level: ${
             courseData.Teacher && courseData.Teacher.jp_level
           }`}</Text>
         </View>
         <View style={{ flex: 1 }}>
-          <Text style={{ fontSize: 16, margin: 5 }}>{`経験: ${
+          <Text>{`Experience: ${
             courseData.Teacher && courseData.Teacher.experience
           }`}</Text>
-          <Text style={{ fontSize: 16, margin: 5 }}>{`誕生日: ${
+          <Text>{`Date of Birth: ${
             courseData.Teacher && courseData.Teacher.User.dob
           }`}</Text>
         </View>
       </View>
 
-      <Text
-        style={{
-          marginTop: 10,
-          marginLeft: "auto",
-          fontSize: 25,
-          color: "green",
-          fontWeight: "bold",
-        }}
-      >{`価格: $${courseData.price}`}</Text>
-      <TouchableOpacity
-        style={{
-          padding: 10,
-          backgroundColor: "#0961f5",
-          borderRadius: 5,
-          alignItems: "center",
-          marginTop: 50,
-        }}
-        onPress={() => enrollNow()}
-      >
-        <Text style={{ color: "white", fontSize: 18, fontWeight: "bold" }}>
-          今すぐ登録
-        </Text>
-      </TouchableOpacity>
+      <Text style={{ marginTop: 10 }}>{`Price: $${courseData.price}`}</Text>
+
+      {isPayment == false ? <Button title="Enroll now" onPress={enrollNow} /> : <Button title="Learn" onPress={enrollNow} />}
     </View>
   );
 };
